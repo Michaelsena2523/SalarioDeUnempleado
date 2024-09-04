7c8a36f675841bd1b8a064c7e040c5947d949ae0
Michael Sena <64752026+Michaelsena2523@users.noreply.github.com>
2024-09-04 03:09:41 +0200

    SalarioDeUnempleado
    SalarioDeUnempleado Michael sena monegro

---
+++ app.js
@@ -0,0 +1,8 @@
+const salarioPorHora = prompt("ingrese la hora del salario")
+const horasPorSemana = prompt("ingrese precio de la hora")
+
+// Cálculo del salario mensual
+const totaldelsalario = salarioPorHora  *  horasPorSemana
+
+
+alert("El salario mensual del empleado es: $$" + totaldelsalario);
